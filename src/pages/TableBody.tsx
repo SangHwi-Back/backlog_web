81224b4572f1e2ff05778631b598ce3a041c798b
SangHwi-Back <sanghwiback@gmail.com>
2024-06-12 22:28:04 +0900

    Add Insert Function
    - Data seems strange

--- src/pages/TableBody.tsx
+++ src/pages/TableBody.tsx
@@ -4,10 +4,11 @@ import { type TestData } from "@/pages/data";
 import Link from "next/link";
 
 export default function TableBody({data}: { data: TestData }) {
+    const className: string = 'text-center border-r-2 border-gray-500';
     return (
         <tr key={data.key} className={'border'}>
-            <td className={'text-center border-r-2 border-gray-500'}>{data.key}</td>
-            <td className={'text-center border-r-2 border-gray-500'}><Link href={`detail/${data.key}`}> {data.title} </Link></td>
+            <td className={className}>{data.key}</td>
+            <td className={className}><Link href={`detail/${data.key}`}> {data.title} </Link></td>
             <td className={'text-center'}>{data.date.toDateString()}</td>
         </tr>
     )
--- src/pages/insert/index.tsx
+++ src/pages/insert/index.tsx
@@ -1,11 +1,35 @@
 import CommonInput from "@/components/CommonInput";
+import {TestData, TESTDATA} from "@/pages/data";
+import {FormEvent, useRef} from "react";
+import {useRouter} from "next/navigation";
 
 export default function Index() {
+    const formRef = useRef<HTMLFormElement>(null);
+    const router = useRouter();
+
+    function insertTestData(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const title = formRef.current?.elements.namedItem('title')?.toString() ?? "";
+        const description = formRef.current?.elements.namedItem('description')?.toString() ?? "";
+        const dateString = formRef.current?.elements.namedItem('date')?.toString();
+
+        let data: TestData = {
+            key: TESTDATA.length,
+            title: title,
+            description: description,
+            date: dateString ? new Date(dateString) : new Date()
+        };
+
+        TESTDATA.push(data);
+        router.push('/');
+    }
+
     return (
-        <div className={'w-[400px]'}>
+        <form onSubmit={insertTestData} className={'w-[400px]'} ref={formRef}>
             <label htmlFor="title">TITLE</label>
             <p className={'mb-4'}>
-                <CommonInput type={'text'} placeholder={'Title'} name={'title'} id={'title'}/>
+                <CommonInput type={'text'} placeholder={'Title'} name={'title'} id={'title'} value={'test'}/>
             </p>
             <label htmlFor="description">DESCRIPTION</label>
             <p className={'mb-4'}>
@@ -13,12 +37,15 @@ export default function Index() {
                     className={'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
                     id={'description'}
                     name={'description'}
-                    placeholder={'description'}/>
+                    placeholder={'description'}
+                    value={'test'}
+                />
             </p>
             <label htmlFor="date">DATE</label>
             <p>
                 <CommonInput type={'date'} placeholder={'DATE'} name={'date'} id={'date'}/>
             </p>
-        </div>
+            <button type={'submit'}>Submit</button>
+        </form>
     )
 }
