d4597f7855b73fd405b65cdce698e78a61d1a9d3
SangHwi-Back <sanghwiback@gmail.com>
2024-06-23 13:08:34 +0900

    Remove unused file

--- app/ui/BlogListItem.tsx
+++ app/ui/BlogListItem.tsx
@@ -2,10 +2,6 @@
 
 import type { BlogRow } from "../lib/dto";
 
-export default function BlogListItem({item}: {item: BlogRow | null}) {
-    if (item === null) {
-        return <div>Loading...</div>
-    }
-
+export default function BlogListItem({item}: {item: BlogRow}) {
     return <li key={item.key}>{item.title}</li>;
 }
