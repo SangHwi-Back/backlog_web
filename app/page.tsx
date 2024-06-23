10fe8ae52c54cf8af9cf13e763d77352ce78d1c1
SangHwi-Back <sanghwiback@gmail.com>
2024-06-23 16:46:00 +0900

    Convert '/' as server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+'use server';
+
 import BlogList from "./ui/BlogList";
 import Main from "./ui/Main";
 import ListItemSkeleton from "./ui/ListItemSkeleton";
 import {Suspense} from "react";
 
-export default function Page() {
+export default async function Page() {
     return <Main>
         <Suspense fallback={<ListItemSkeleton title={'waiting'}/>}>
             <BlogList/>
