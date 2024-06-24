92057d1c24f138e8329dc9ee0c37efc90ab0e51f
sanghwiback <sanghwiback@gmail.com>
2024-06-24 11:02:29 +0900

    Add POSTGRES_URL in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    POSTGRES_URL: process.env.POSTGRES_URL,
+  }
 };
 
 export default nextConfig;
