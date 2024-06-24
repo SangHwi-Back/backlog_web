b1cc7bf9213f28f1f9ae345b14f467d18d14514f
sanghwiback <sanghwiback@gmail.com>
2024-06-24 12:21:51 +0900

    Add webpack config at next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     POSTGRES_URL: process.env.POSTGRES_URL,
-  }
+  },
+  webpack(config) {
+    const prod = process.env.NODE_ENV === 'production';
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      plugins: [...config.plugins],
+    }
+  },
 };
 
 export default nextConfig;
