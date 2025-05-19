67b4182435f08bbba728cb041ae15f1a8a28613c
sanghwiback <sanghwiback@gmail.com>
2025-05-19 10:37:57 +0900

    Refactor not-found page styling and cleanup globals.css.
    Updated the styling for the not-found page, including improved class usage and link correction. Removed unused CSS variables from globals.css to streamline the codebase.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,11 @@
-'use client';
-
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className={'text-black text-center'}>
+    // <div style={{ color: 'black' }} className={'text-center'}>
+    <div className={'!text-black text-center'}>
       <h2>Unwanted error occurred 😩</h2>
-      <Link className={'underline'} href="/backlog_web/public">Return Home</Link>
+      <Link className={'underline'} href="/">Return Home</Link>
     </div>
   )
 }
